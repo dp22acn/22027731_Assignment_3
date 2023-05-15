58bec65d2b67f9feb86c8bda29bf119a4140dcdb
dp22acn <devendrapeddoju@gmail.com>
2023-05-15 16:56:26 +0100

    Importing Nessecery Libraries

---
+++ 22027731.py
@@ -0,0 +1,10 @@
+# Importing Nessecery Libraries
+import warnings
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+from scipy.optimize import curve_fit
+from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+from sklearn.preprocessing import MinMaxScaler
